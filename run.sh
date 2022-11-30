6ec5452f83bbb8f07892d3880b079de6354e1387
ddkozyreva <ddkozyreva@edu.hse.ru>
2022-11-30 22:52:40 +0300

    some fixes in readme and bash-script

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
 
 #!/bin/bash
-echo "2.3b"
-g++ -O3 -fopenmp 2.3b-fourier.cpp -o filename
-for (( num_threads=1; num_threads <= 8; num_threads++ ))
-do
-    export OMP_NUM_THREADS=$num_threads; ./filename < input.txt
-done
 
 echo "2.2a"
 g++ -O3 -fopenmp 2.2a-buffon_pi.cpp -o filename
+echo "With different thread's number"
 for (( num_threads=1; num_threads <= 8; num_threads++ ))
 do
     echo "number of cpu's:"
@@ -17,10 +12,18 @@ do
     echo "42000000" | ./filename
 done
 
-echo "____"
+echo "With different input number"
 for (( numbers=1000; numbers <= 1000000000; numbers*=10 ))
 do
     echo "Input number is $numbers"
     export OMP_NUM_THREADS=8
     echo $numbers | ./filename
+done
+
+
+echo "2.3b"
+g++ -O3 -fopenmp 2.3b-fourier.cpp -o filename
+for (( num_threads=1; num_threads <= 8; num_threads++ ))
+do
+    export OMP_NUM_THREADS=$num_threads; ./filename < input.txt
 done
