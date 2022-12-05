eebf38d3f293a418b492822b2ed5e1c57fe669ec
ddkozyreva <ddkozyreva@edu.hse.ru>
2022-12-05 23:51:14 +0300

    laplace

--- laplace2d.cpp
+++ laplace2d.cpp
@@ -11,38 +11,53 @@
 using ptrdiff_t = std::ptrdiff_t;
 using size_t = std::size_t;
 
-function random_bounds(matrix<double> u, ptrdiff_t ncenters, double ampl, size_t seed)
+void random_filling(matrix<double> u, double ampl, size_t seed) {
+    std::mt19937_64 rng(seed);
+    std::uniform_real_distribution<double> dist_charge(-ampl, ampl);
+    for (ptrdiff_t i = 1; i < u.nrows() - 1; i++)
+	{
+		for (ptrdiff_t j = 1; j < u.ncols() - 1; j++)
+			u(i, j) = dist_charge(rng);
+	}
+}
+void single_filling(matrix<double> u) {
+    for (ptrdiff_t i = 0; i < u.nrows(); i++)
+	{
+		for (ptrdiff_t j = 0; j < u.ncols(); j++)
+			u(i, j) = 1.0;
+	}
+}
+void random_bounds(matrix<double> u, ptrdiff_t ncenters, double ampl, size_t seed)
 {
     std::mt19937_64 rng(seed);
-    std::uniform_real_distribution<double> dist_charge(-ampl, ampl_max);
+    std::uniform_real_distribution<double> dist_charge(-ampl, ampl);
     std::uniform_real_distribution<double> dist_coord(-1, 1);
-    
     ptrdiff_t generated=0, nx = u.nrows(), ny = u.ncols();
     double hx = 1.0 / (nx-1), hy = 1.0 / (ny -1);
     while (generated < ncenters)
     {
         double x = dist_coord(rng), y = dist_coord(rng);
-        if ((abs(x) > 0.5) && (abs(y) > 0.5))
+        if ((std::abs(x) > 0.5) && (std::abs(y) > 0.5))
         {
             double charge = dist_charge(rng);
-            for (ptrdiff_t i=0, i < nx; i++)
+            for (ptrdiff_t i=0; i < nx; i++)
             {
                 u(i, 0) = 0;
                 u(i, ny-1) = 0;
             }
-            for (ptrdiff_t j=1, i < ny-1; j++)
+            for (ptrdiff_t j=1; j < ny-1; j++)
             {
                 u(0, j) = 0;
                 u(nx-1, j) = 0;
             }
-            for (ptrdiff_t i=0, i < nx; i++)
+            for (ptrdiff_t i=0; i < nx; i++)
             {
                 double r = hypot(x - (-0.5 + i * hx), y + 0.5);
                 u(i, 0) += charge / r;
                 r = hypot(x - (-0.5 + i * hx), y - 0.5);
                 u(i, ny-1) += charge / r;
             }
-            for (ptrdiff_t j=1, i < ny-1; j++)
+            for (ptrdiff_t j=1; j < ny-1; j++)
             {
                 double r = hypot(x + 0.5, y - (-0.5 + j * hy));
                 u(0, j) += charge / r;
@@ -53,12 +68,6 @@ function random_bounds(matrix<double> u, ptrdiff_t ncenters, double ampl, size_t
         }
     }
 }
-
-void laplace2d(matrix<double> u, double hx, double hy)
-{
-    
-}
-
 int test_laplace(matrix<double> u, double atol)
 {
     ptrdiff_t nx = u.nrows(), ny = u.ncols();
@@ -66,7 +75,7 @@ int test_laplace(matrix<double> u, double atol)
     {
         for (ptrdiff_t j = 1; j < ny-1; j++)
         {
-            if (abs(u(i-1, j) + u(i+1, j) + u(i, j-1) + u(i, j+1) - 4 * u(i, j)) > atol)
+            if (std::fabs(u(i-1, j) + u(i+1, j) + u(i, j-1) + u(i, j+1) - 4 * u(i, j)) > atol)
             {
                 return 0;
             }
@@ -74,16 +83,32 @@ int test_laplace(matrix<double> u, double atol)
     }
     return 1;
 }
+void laplace2d(matrix<double> u, double hx, double hy)
+{
+    ptrdiff_t j;
+    matrix<double> u_next = u;
+
+    while(!test_laplace(u, 1e-6)) {
+        #pragma omp parallel for private(j)
+        for (ptrdiff_t i = 0; i < u.nrows() - 1; i++) {
+            for (j = 0; j < u.ncols() - 1; j++) {
+                u_next(i, j) = 0.25 * (u(i-1, j) + u(i+1,j) + u(i,j-1) + u(i, j+1));
+            }
+        }
+        #pragma omp critical
+        {
+            u = u_next;
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
-    ptrdiff_t n;
-
-    std::cin >> n;
-    matrix<double> u(n, n);
-
+    ptrdiff_t n, m;
+    std::cin >> n >> m;
+    matrix<double> u(n, m);
+    random_filling(u, 10.0, 9876);
     random_bounds(u, 100, 5.0, 9876);
-
 
     double t0 = omp_get_wtime();
 
